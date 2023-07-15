7f41743da734144727068a669393267c031e4c95
ZwerOxotnik <zweroxotnik@gmail.com>
2023-07-15 18:10:39 +0300

    Update to 0.1.3

--- scenarios/MOBA-ZO/bonus_choices.lua
+++ scenarios/MOBA-ZO/bonus_choices.lua
@@ -1,4 +1,4 @@
-return {
+local choices = {
 	{{name = "power-armor", count = 1}},
 	{
 		{name = "flamethrower", count = 1},
@@ -26,8 +26,8 @@ return {
 	{{name="slowdown-capsule", count = 10}},
 	{{name="poison-capsule", count = 10}},
 	{{name="grenade", count = 20}},
-	{{name="land-mine", count = 20}},
 	{{name="cluster-grenade", count = 5}},
+	{{name="land-mine", count = 20}},
 	{{name="laser-turret", count = 2}},
 	{{name = "gun-turret", count = 10}},
 	{
@@ -56,13 +56,113 @@ return {
 	},
 	{{name="rocket", count = 5}},
 	{{name="explosive-rocket", count = 1}},
-
-	--- From IndustrialRevolution3
-	{{name="scatterbot-capsule", count = 1}},
-	{{name="steel-plate-wall", count = 100}},
-	{{name="arc-turret", count = 1}},
-	{{name="photon-turret", count = 1}},
-	{{name="scattergun-turret",  count = 5}},
-	{{name="medical-pack", count = 4}},
-	{{name="bronze-cartridge", count = 10}},
 }
+
+if script.active_mods.IndustrialRevolution3 then
+	choices[#choices+1] = {{name="scatterbot-capsule", count = 1}}
+	choices[#choices+1] = {{name="steel-plate-wall", count = 100}}
+	choices[#choices+1] = {{name="arc-turret", count = 1}}
+	choices[#choices+1] = {{name="photon-turret", count = 1}}
+	choices[#choices+1] = {{name="scattergun-turret",  count = 5}}
+	choices[#choices+1] = {{name="medical-pack", count = 4}}
+	choices[#choices+1] = {{name="bronze-cartridge", count = 10}}
+end
+
+if script.active_mods.Repair_Turret then
+	choices[#choices+1] = {{name="repair-turret", count = 3}}
+end
+
+if script.active_mods["shield-projector"] then
+	choices[#choices+1] = {{name="shield-projector", count = 1}}
+end
+
+if script.active_mods.stasis_mine then
+	choices[#choices+1] = {{name="stasis-land-mine", count = 1}}
+end
+
+if script.active_mods.laserfence then
+	choices[#choices+1] = {
+		{name="laserfence-connector", count = 20},
+		{name="laserfence-connector-cage", count = 10}
+	}
+end
+
+if script.active_mods.bobwarfare then
+	choices[#choices+1] = {
+		{name="reinforced-wall", count = 90},
+		{name="reinforced-gate", count = 10}
+	}
+	choices[#choices+1] = {{name="distractor-mine", count = 3}}
+	choices[#choices+1] = {{name="poison-mine", count = 6}}
+	choices[#choices+1] = {{name="slowdown-mine", count = 12}}
+end
+
+if script.active_mods.Kombat_Drones then
+	choices[#choices+1] = {{name="infantry-depot", count = 1}}
+	choices[#choices+1] = {{name="basic-infantry", count = 10}}
+	choices[#choices+1] = {{name="heavy-infantry", count = 10}}
+	choices[#choices+1] = {{name="power-infantry", count = 1}}
+end
+
+if script.active_mods["baron-turrets"] then
+	choices[#choices+1] = {
+		{name="curtain-turret", count = 1},
+		{name="sulfuric-acid-barrel", count = 10},
+		{name="assembling-machine-2", count = 1},
+	}
+	choices[#choices+1] = {
+		{name="cannon-turret", count = 2},
+		{name="grenade-belt", count = 40},
+	}
+end
+
+if script.active_mods["Reinforced-Walls"] then
+	choices[#choices+1] = {
+		{name="reinforced-wall", count = 90},
+		{name="reinforced-gate", count = 10}
+	}
+	choices[#choices+1] = {
+		{name="acid-resist-wall", count = 60},
+		{name="acid-resist-gate", count = 10}
+	}
+	choices[#choices+1] = {
+		{name="damage-reflect-wall", count = 40},
+		{name="damage-reflect-gate", count = 10}
+	}
+end
+
+if script.active_mods["Additional-Turret-updated"] then
+	choices[#choices+1] = {{name="at_LC_b", count = 3}}
+	choices[#choices+1] = {{name="at-cannon-turret-mk1", count = 3}}
+end
+
+if script.active_mods.Krastorio2 then
+	choices[#choices+1] = {{name="kr-biter-virus", count = 3}}
+	choices[#choices+1] = {{name="kr-creep-virus", count = 3}}
+end
+
+if script.active_mods.RampantArsenal then
+	choices[#choices+1] = {{name="incendiary-landmine-rampant-arsenal", count = 6}}
+	choices[#choices+1] = {{name="bio-landmine-rampant-arsenal", count = 3}}
+	choices[#choices+1] = {{name="he-landmine-rampant-arsenal", count = 1}}
+	choices[#choices+1] = {{name="bio-grenade-capsule-rampant-arsenal", count = 1}}
+	choices[#choices+1] = {{name="incendiary-grenade-capsule-rampant-arsenal", count = 1}}
+	choices[#choices+1] = {{name="toxic-capsule-rampant-arsenal", count = 1}}
+	choices[#choices+1] = {{name="paralysis-capsule-rampant-arsenal", count = 1}}
+	choices[#choices+1] = {{name="repair-capsule-rampant-arsenal", count = 2}}
+	choices[#choices+1] = {{name="healing-capsule-rampant-arsenal", count = 1}}
+	choices[#choices+1] = {{name="speed-capsule-rampant-arsenal", count = 1}}
+	choices[#choices+1] = {{name="mending-wall-rampant-arsenal", count = 100}}
+	choices[#choices+1] = {{name="reinforced-wall-rampant-arsenal", count = 50}}
+	choices[#choices+1] = {{name="shotgun-ammo-turret-rampant-arsenal", count = 4}}
+	choices[#choices+1] = {{name="cannon-ammo-turret-rampant-arsenal",  count = 1}}
+	choices[#choices+1] = {
+		{name="medic-ammo-turret-rampant-arsenal", count = 1},
+		{name="self-repair-capsule-ammo-rampant-arsenal", count = 2},
+	}
+	choices[#choices+1] = {{name="lightning-electric-turret-rampant-arsenal", count = 1}}
+	choices[#choices+1] = {{name="capsule-ammo-turret-rampant-arsenal", count = 2}}
+end
+
+
+return choices
--- scenarios/MOBA-ZO/start_techs.lua
+++ scenarios/MOBA-ZO/start_techs.lua
@@ -1,4 +1,4 @@
-return {
+local techs = {
 	"military",
 	"logistics",
 	"stone-wall",
@@ -30,28 +30,47 @@ return {
 	"rail-signals",
 	"logistic-science-pack",
 	"circuit-network",
-
-	--- From IndustrialRevolution3
-	"ir-charcoal",
-	"ir-grinding",
-	"ir-grinding-2",
-	"ir-steambot",
-	"ir-bronze-furnace",
-	"ir-bronze-forestry",
-	"ir-iron-forestry",
-	"ir-research-1",
-	"ir-gold-milestone",
-	"ir-iron-milestone",
-	"ir-steel-milestone",
-	"ir-crude-oil-processing",
-	"ir-light-oil-processing",
-	"ir-heavy-oil-processing",
-	"ir-lampbot",
-	"ir-graphite",
-	"ir-silicon",
-	"ir-steam-power",
-	"ir-petro-generator",
-	"ir-electronics-1",
-	"ir-iron-motor",
-	"ir-coking",
 }
+
+if script.active_mods.Kombat_Drones then
+	techs[#techs+1] = "infantry-depot"
+end
+
+if script.active_mods.Krastorio2 then
+	techs[#techs+1] = "kr-greenhouse"
+	techs[#techs+1] = "kr-stone-processing"
+	techs[#techs+1] = "kr-crusher"
+	techs[#techs+1] = "kr-automation-core"
+	techs[#techs+1] = "kr-steam-engine"
+	techs[#techs+1] = "kr-electric-mining-drill"
+	techs[#techs+1] = "kr-fluids-chemistry"
+	techs[#techs+1] = "kr-silicon-processing"
+end
+
+if script.active_mods.IndustrialRevolution3 then
+	techs[#techs+1] = "ir-charcoal"
+	techs[#techs+1] = "ir-grinding"
+	techs[#techs+1] = "ir-grinding-2"
+	techs[#techs+1] = "ir-steambot"
+	techs[#techs+1] = "ir-bronze-furnace"
+	techs[#techs+1] = "ir-bronze-forestry"
+	techs[#techs+1] = "ir-iron-forestry"
+	techs[#techs+1] = "ir-research-1"
+	techs[#techs+1] = "ir-gold-milestone"
+	techs[#techs+1] = "ir-iron-milestone"
+	techs[#techs+1] = "ir-steel-milestone"
+	techs[#techs+1] = "ir-crude-oil-processing"
+	techs[#techs+1] = "ir-light-oil-processing"
+	techs[#techs+1] = "ir-heavy-oil-processing"
+	techs[#techs+1] = "ir-lampbot"
+	techs[#techs+1] = "ir-graphite"
+	techs[#techs+1] = "ir-silicon"
+	techs[#techs+1] = "ir-steam-power"
+	techs[#techs+1] = "ir-petro-generator"
+	techs[#techs+1] = "ir-electronics-1"
+	techs[#techs+1] = "ir-iron-motor"
+	techs[#techs+1] = "ir-coking"
+end
+
+
+return techs
